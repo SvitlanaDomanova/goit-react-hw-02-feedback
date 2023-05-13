7b61e55693cc6cae66ce4e58e4ff8f8adc9f5e69
SvitlanaDomanova <majba161@gmail.com>
2023-05-13 13:52:56 +0300

    final

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,59 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import Section from './Section/Section';
+import FeedbackOptions from './FeedbackOptions/FeedbackOptions'
+import Statistscs from './Statistics/Statistics';
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleFeedback = option => {
+    this.setState(prevState => {
+      return {
+        [option]: prevState[option] + 1,
+      };
+    });
+  };
+  countTotalFeedback = () => {
+    return this.state.good + this.state.neutral + this.state.bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    return (this.state.good / this.countTotalFeedback()) * 100;
+  };
+
+  render() {
+    const stateItemNames = Object.keys(this.state);
+    const total = this.countTotalFeedback();
+    const positivePercent = this.countPositiveFeedbackPercentage().toFixed();
+
+    return (
+      <div
+        style={{
+          marginLeft: '15px',
+        }}
+      >
+        <Section title="Please leave feedback">
+          {/* <FeedbackOptions
+            handleIncrementGood={this.handleIncrementGood}
+            handleIncrementNeutral={this.handleIncrementNeutral}
+            handleIncrementBad={this.handleIncrementBad}
+          /> */}
+          <FeedbackOptions options={stateItemNames} onLeaveFeedback={this.handleFeedback}></FeedbackOptions>
+          <Statistscs
+            title={'Statistics'}
+            good={this.state.good}
+            neutral={this.state.neutral}
+            bad={this.state.bad}
+            total={total}
+            positivePercentage={positivePercent}
+          />
+        </Section>
+      </div>
+    );
+  }
+}
+export default App;
